92eedf8f22ac89a7f6d26b21f9834f3b23d970b1
Viktor <viktor.lugovskoy@yahoo.com>
2022-04-06 07:34:19 +0300

    13 Функции, стрелочные функции

--- js/script.js
+++ js/script.js
@@ -7,12 +7,35 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-			b = prompt("На сколько оцените его?", ""),
-			c = prompt("Один из последних просмотренных фильмов?", ""),
-			d = prompt("На сколько оцените его?", "");
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//   b = prompt("На сколько оцените его?", ""),
+//   c = prompt("Один из последних просмотренных фильмов?", ""),
+//   d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("Done!");
+  } else {
+    console.log("Error");
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+	console.log("Мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+	console.log("Классический зритель");
+} else if (personalMovieDB.count >= 30) {
+	console.log("Киноман");
+} else {
+	console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
---
+++ js/script13.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let num = 20;
+
+function showFirstMessage(text) {
+  console.log(text);
+  num = 10;
+  console.log(num);
+}
+
+showFirstMessage("Hello world!");
+console.log(num);
+
+// function calc(a, b) {
+// 	return a + b;
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+function ret() {
+  let num = 50;
+  return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function () {
+  console.log("Hello!");
+};
+
+logger();
+
+const calc = (a, b) => {
+	console.log('1');
+  return a + b;
+};
